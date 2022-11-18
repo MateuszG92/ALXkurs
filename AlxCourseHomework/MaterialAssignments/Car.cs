63cf862c6bcccfe6c3be193ffa1864aa1b01c6b0
MateuszG92 <mateuszg00@gmail.com>
2022-11-18 21:44:51 +0100

    Homework

---
+++ AlxCourseHomework/MaterialAssignments/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlxCourseHomework.MaterialAssignments
+{
+    public class Car 
+    {
+        public string Brand;
+        public double NettoPrice;
+        public double InventoryNettoPrice;
+        public double Tax;
+        public double Price;
+        public Car()
+        {
+
+        }
+        public Car(string brand, double nettoPrice, double inventoryPrice, double tax)
+        {
+            Brand = brand;
+            NettoPrice = nettoPrice;
+            InventoryNettoPrice = inventoryPrice;
+            Tax = tax;
+        }
+        public double CarBruttoPrice()
+        {
+            return (NettoPrice+InventoryNettoPrice)*(1+Tax);
+        }
+        public void Present()
+        {
+            Console.WriteLine("Car info");
+            Console.WriteLine($"Car brand: {Brand}");
+            Console.WriteLine($"Car netto price: {NettoPrice}");
+            Console.WriteLine($"Car inventory price: {InventoryNettoPrice}");
+            Console.WriteLine("Tax:" + (NettoPrice+InventoryNettoPrice)*Tax);
+        }
+    }
+}
---
+++ AlxCourseHomework/MaterialAssignments/ExercisesPage45.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlxCourseHomework.MaterialAssignments;
+
+namespace AlxCourseHomework.MaterialAssignments
+{
+    public class ExercisesPage45
+    {
+        public static void Run41()
+        {
+            string price;
+            Car car1 = new Car("BMW", 50000, 20000, 0.23);
+            car1.Present();
+            price=car1.CarBruttoPrice().ToString();
+            Console.WriteLine($"Car brutto price: " + price);
+            Car car2 = new Car("Audi", 40000, 20000, 0.09);
+            car2.Present();
+            price = car2.CarBruttoPrice().ToString();
+            Console.WriteLine($"Car brutto price: " + price);
+            Car car3 = new Car("Mercedes", 60000, 40000, 0.19);
+            car3.Present();
+            price = car3.CarBruttoPrice().ToString();
+            Console.WriteLine($"Car brutto price: " + price);
+        }
+        public static void Run42()
+        {
+            Console.Write("Enter your name (write END to exit): ");
+            var name = Console.ReadLine();
+            while (name.ToLower() != "end")
+            {
+                char lastLetter = name[name.Length - 1];
+                if (lastLetter == 'a')
+                {
+                    Console.WriteLine("Hello Mrs. " + name);
+                    Console.Write("Enter your name (write END to exit): ");
+                    name = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Hello Mr. " + name);
+                    Console.Write("Enter your name (write END to exit): ");
+                    name = Console.ReadLine();
+                }
+            }
+            Console.WriteLine("End");
+        }
+    }
+}
